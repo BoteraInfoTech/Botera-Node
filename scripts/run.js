a737cc7068b7c8ec5c0feb5d3b3af7a7ed148e54
Akil Saiyad <akil@socialpilot.co>
2025-09-12 00:21:02 +0530

    Update start script in package.json to use node instead of sh, and set default service name in run.js to 'rest'

--- scripts/run.js
+++ scripts/run.js
@@ -10,7 +10,7 @@ const runner = () => {
 
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const serviceName = process.env.SERVICE || '';
+  const serviceName = process.env.SERVICE || 'rest';
 
   const servicePath = path.join(
     __dirname,
